7d8821a222ab2237250155bf357bf1bd85962fa2
OteroG-27 <coterocarranza@gmail.com>
2023-06-21 13:36:56 -0500

    vista de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
 import { routingComponents } from './app-routing.module';
+import { ReactiveFormsModule,  FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,10 @@ import { routingComponents } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servicios/api/api.service.ts
+++ src/app/servicios/api/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LoginI } from 'src/app/modelos/login.interface';
+import { ResponseI } from 'src/app/modelos/response.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  url:string = "https://lab.app.invertebrado.co/api/";
+
+  constructor(private http:HttpClient) { }
+  loginByEmail(form:LoginI):Observable<ResponseI>{
+    let direccion = this.url + "account/login/";
+    return this.http.post<ResponseI>(direccion,form)
+  }
 }
